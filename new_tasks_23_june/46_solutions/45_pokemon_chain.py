cd8ae4df7c5ef10345e586a36523e1a1c817c33c
vishal-k3gster <vishal.k3gster@gmail.com>
2017-06-28 17:45:03 +0530

    working on pokemon_name_chain problem.

--- new_tasks_23_june/46_solutions/45_pokemon_chain.py
+++ new_tasks_23_june/46_solutions/45_pokemon_chain.py
@@ -2,6 +2,60 @@
 
 # Your task in this exercise is as follows: Take the following selection of 70 English Pokemon names (extracted from Wikipedia's list of Pokemon) and generate the/a sequence with the highest possible number of Pokemon names where the subsequent name starts with the final letter of the preceding name. No Pokemon name is to be repeated.
 
-fp = open("pokemon_name_list.txt","r")
-ls = fp.read().split("#")[0].split()
-print ls
+
+def get_name_list(file_name):
+    fp = open(file_name,"r")
+    #    ls = fp.read().split("#")[0].split()
+    ls = fp.read().replace("#","\n").split()
+    fp.close()
+    print ls,"\n\n"
+    return ls
+
+def create_char_dict(name_list):
+    char_dict = {}
+    for name in name_list:
+        if name[-1] not in char_dict:
+            char_dict[name[-1]] = [{},{name}]
+        elif char_dict[name[-1]][1] == {} :
+            char_dict[name[-1]][1] = {name}
+        else:
+            char_dict[name[-1]][1].add(name)
+
+
+        if name[0] not in char_dict:
+            char_dict[name[0]] = [{name},{}]
+        elif char_dict[name[-1]][0] == {}:
+            char_dict[name[0]][0] = {name}
+        else:
+            char_dict[name[-1]][0].add(name)
+
+    return char_dict
+
+def get_chain(name,char_dict,name_list, sequence):
+    if char_dict[name[-1]][0] != {}:
+        chain_list = []
+        for new in char_dict[name[-1]][0]:
+            if new in sequence:
+                continue
+            chain_list.append( get_chain(new,char_dict,name_list,sequence.append(new)))
+        if chain_list == []:
+            return []
+        return max(chain_list, key = len)
+    return sequence
+
+        
+    
+
+def longest_name_chain():
+    name_list = get_name_list("pokemon_name_list.txt")
+    char_dict = create_char_dict(name_list)
+    my_dict = {}
+    for i in name_list:
+        my_dict[i] = get_chain(i,char_dict,name_list,sequence = [i])
+    return my_dict
+
+d = longest_name_chain()
+for i in d.items():
+    print i
+
+print "\n\n",max(d.values(), key = len)
