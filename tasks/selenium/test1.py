a62c8db3e22f9b120f40561f27d550803f3fb864
vishal-k3gster <vishal.k3gster@gmail.com>
2017-07-06 12:54:52 +0530

    done some changes in selenium automation form filling. sending for review.

--- tasks/selenium/test1.py
+++ tasks/selenium/test1.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
-# driver = webdriver.Chrome()
+# driver = webdriver.Firefox()
+driver = webdriver.Chrome(executable_path = '/usr/local/bin/chromedriver')
 driver.get("http://toolsqa.com/automation-practice-form/")
-assert "Demo" in driver.title
-elem = driver.find_element_by_name("firstname")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
+# assert "Demo" in driver.title
+# elem = driver.find_element_by_name("firstname")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
 driver.close()
--- tasks/selenium/test2.py
+++ tasks/selenium/test2.py
@@ -1,45 +1,57 @@
-import unittest, time
+import unittest, time, os
 from selenium import webdriver
+
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
 
-class PythonOrgSearch(unittest.TestCase):
+class SeleniumFormAutomation(unittest.TestCase):
 
     def setUp(self):
+        
         self.driver = webdriver.Firefox()
+        # self.driver = webdriver.Chrome()
+        self.firstname = "vishal"
+        self.lastname = "chillal"
+        self.form_page = "http://toolsqa.com/automation-practice-form/"
 
     def test_form_filling(self):
+        ''' function to fill and submit the form. '''
         browser = self.driver
-        form_page = "http://toolsqa.com/automation-practice-form/"
-        browser.get(form_page)
+        browser.get(self.form_page)
         assert "Demo" in browser.title
-
-        browser.find_element_by_name("firstname").send_keys("vishal")
-        browser.find_element_by_name("lastname").send_keys("chillal")
+        browser.find_element_by_name("firstname").send_keys(self.firstname)
+        browser.find_element_by_name("lastname").send_keys(self.lastname)
         browser.find_element_by_id("sex-0").click()
         browser.find_element_by_id("exp-0").click()
+        
+        calander = browser.find_element_by_id("datepicker")
+        ActionChains(self.driver).move_to_element(calander).click().send_keys('01012011').perform()
+
         browser.find_element_by_id("profession-1").click()
+        browser.find_element_by_id("photo").send_keys(os.getcwd()+"/image.png")
         browser.find_element_by_id("tool-1").click()
         browser.find_element_by_id("tool-2").click()
+        
         continent = Select(browser.find_element_by_id("continents"))
         continent.select_by_visible_text('Antartica')
-
+        
         browser.find_element_by_id("submit").click()
 
-        wait = WebDriverWait( browser, 5 )
-        print browser.current_url
+    def test_check_submission(self):
+        ''' in this function we can check the aftersubmit oprations.'''
+        wait = WebDriverWait( self.driver, 5 )
+        url = self.driver.current_url
 
         try:
             page_loaded = wait.until_not(
-                lambda browser: browser.current_url == form_page
+                lambda browser: self.driver.current_url == self.form_page
             )
         except TimeoutException:
             self.fail( "Loading timeout expired" )
- 
 
     def tearDown(self):
-        time.sleep(3)
         self.driver.close()
 
 if __name__ == "__main__":
