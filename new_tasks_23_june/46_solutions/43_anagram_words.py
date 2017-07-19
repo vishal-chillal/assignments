14596bbbdff108622a3a14147d75f98865583d67
vishal-k3gster <vishal.k3gster@gmail.com>
2017-07-19 14:13:28 +0530

    optimised the anagram finding code using sorting the word and using dictionary.

--- new_tasks_23_june/46_solutions/43_anagram_words.py
+++ new_tasks_23_june/46_solutions/43_anagram_words.py
@@ -13,40 +13,50 @@ def get_words_from_file(file_name):
         print e
     return word_list
 
-def find_anagram(char_dict,word):
-    anagram_list = {}
-    if len(word) != 1:
-        for char in word:
-            tmp_lst = set(filter(lambda x:x.count(char) == word.count(char) and  len(x) == len(word) ,char_dict[char]))
-            if anagram_list == {}:
-                anagram_list = tmp_lst
-            else:
-                anagram_list = anagram_list.intersection(tmp_lst)
-    return anagram_list
+# def find_anagram(char_dict,word):
+#     anagram_list = {}
+#     if len(word) != 1:
+#         for char in word:
+#             tmp_lst = set(filter(lambda x:x.count(char) == word.count(char) and  len(x) == len(word) ,char_dict[char]))
+#             if anagram_list == {}:
+#                 anagram_list = tmp_lst
+#             else:
+#                 anagram_list = anagram_list.intersection(tmp_lst)
+#     return anagram_list
 
-def create_dict(word_list):
-    char_dict = {}
-    for word in word_list:
-        for char in word:
-            if char not in char_dict:
-                char_dict[char] = {word}
-            else:
-                char_dict[char].add(word)
-    return char_dict
-    
+# def create_dict(word_list):
+#     char_dict = {}
+#     for word in word_list:
+#         for char in word:
+#             if char not in char_dict:
+#                 char_dict[char] = {word}
+#             else:
+#                 char_dict[char].add(word)
+#     return char_dict
+
 def anagram():
 
     word_list = get_words_from_file("unixdict.txt")
-    char_dict = create_dict(word_list)
-    cnt = -1
-    anagram_word_lst = {1}
+    anagram_word_lst = {}
     for word in word_list:
-        if word not in anagram_word_lst:
-            lst = find_anagram(char_dict, word)
-            if len(lst) > 2:
-                print lst, word
-                anagram_word_lst.update(lst)
-    print anagram_word_lst     
+        sorted_word = "".join(sorted(word))
+        try:
+            anagram_word_lst[sorted_word].append(word)
+        except KeyError:
+            anagram_word_lst[sorted_word] = [word]
+    cnt = 0
+    for _, anagram_list in anagram_word_lst.items():
+        if len(anagram_list) > 1:
+            cnt += 1
+            print anagram_list
+
+    print cnt
+    #     if word not in anagram_word_lst:
+    #         lst = find_anagram(char_dict, word)
+    #         if len(lst) > 2:
+    #             print lst, word
+    #             anagram_word_lst.update(lst)
+    # print anagram_word_lst     
 if __name__ == "__main__":
 
 	anagram()
