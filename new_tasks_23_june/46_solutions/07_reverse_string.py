23fbbebea010786f08fcdf49a2363a5d1b612115
vishal-k3gster <vishal.k3gster@gmail.com>
2017-07-19 14:15:18 +0530

    reverse string using list comprehension and join.

--- new_tasks_23_june/46_solutions/07_reverse_string.py
+++ new_tasks_23_june/46_solutions/07_reverse_string.py
@@ -8,7 +8,12 @@ def rev_using_for_loop(string):
         rev_str = char+rev_str
     return rev_str
 
+def reverse_str(string):
+    return "".join([string[x] for x in range(len(string)-1,-1,-1)])
 if __name__ == "__main__":
+#def main():
     string = raw_input("Enter String: ")
     print reverse(string)
     print rev_using_for_loop(string)
+    print reverse_str(string)
+#main()
