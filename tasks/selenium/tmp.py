263d5338aa4f967f2952cde552fcb7196d018061
vishal-k3gster <vishal.k3gster@gmail.com>
2017-07-27 12:23:48 +0530

    some change

--- tasks/selenium/tmp.py
+++ tasks/selenium/tmp.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+from time import sleep
 import unittest
 from selenium import webdriver
 from selenium.common.exceptions import *
@@ -9,14 +9,16 @@ class TestHomepage(unittest.TestCase):
         self.browser = webdriver.Firefox()
         
     def testTitle(self):
-        self.form_page = "http://demoqa.com/registration/"
+        self.form_page = "https://www.youtube.com/watch?v=yo6kyyX0nBI"
         self.browser.get(self.form_page)
         # date_8[date][yy]: 1992
-        try:
-            self.browser.find_element_by_name("date_8[date][yy]").send_keys("1993")
-        except NoSuchElementException:
-            print "element error"
-
+        # try:
+        # //*[@id="main"]/div[2]/a[2]
+        #     self.browser.find_element_by_id("masthead-search-term").send_keys("pardesiyon se na ankhiya milana\n")
+        #     self.browser.find_element_by_id("search-btn").click()
+        # except NoSuchElementException:
+        #     print "element error"
+        sleep(3)
     # def tearDown(self):
     #     self.browser.quit()
 
